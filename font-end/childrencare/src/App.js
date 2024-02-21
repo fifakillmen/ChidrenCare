5ed14465b6baeb15a8e0a5a6d35b57eb68cbf24b
sonbn <fifakillmen@gmail.com>
2024-02-21 09:00:19 +0700

    update font-end library router url in font-end

--- font-end/childrencare/src/App.js
+++ font-end/childrencare/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignupComponent from './components/SignupComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+        <Routes>
+          {/* localhost:3000 */}
+          <Route path='/signup' element={<SignupComponent />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+//    <signupComponent/>
