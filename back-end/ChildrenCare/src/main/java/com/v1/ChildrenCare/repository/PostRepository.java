2c0d8c5c6f6941b1de2dbdc72f9f47ea94788a61
AnhTuannn1 <jackienguyennn01@gmail.com>
2024-02-22 20:49:05 +0700

    clear package

--- back-end/ChildrenCare/src/main/java/com/v1/ChildrenCare/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.v1.ChildrenCare.repository;
-
-import com.v1.ChildrenCare.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
